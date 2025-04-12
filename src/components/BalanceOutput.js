15ec1bdb522807da2a3f7f52043ddc364dfa38cc
Miguel Sanchez <miguel.sanchez@devsu.com>
2025-04-12 14:12:02 -0500

    [ADD] Solution in BalanceOutput component connect method

--- src/components/BalanceOutput.js
+++ src/components/BalanceOutput.js
@@ -76,9 +76,69 @@ BalanceOutput.propTypes = {
 };
 
 export default connect(state => {
-  let balance = [];
+  let { startAccount, endAccount, startPeriod, endPeriod } = state.userInput;
 
-  /* YOUR CODE GOES HERE */
+  if (isNaN(startAccount)) {
+    if (state.accounts.length > 0) {
+      startAccount = state.accounts[0].ACCOUNT;
+    }
+  }
+
+  if (isNaN(endAccount)) {
+    if (state.accounts.length > 0) {
+      endAccount = state.accounts[state.accounts.length - 1].ACCOUNT;
+    }
+  }
+
+  if (isNaN(startPeriod)) {
+    if (state.journalEntries.length > 0) {
+      startPeriod = state.journalEntries[0].PERIOD;
+    }
+  }
+
+  if (isNaN(endPeriod)) {
+    if (state.journalEntries.length > 0) {
+      endPeriod = state.journalEntries[state.journalEntries.length - 1].PERIOD;
+    }
+  }
+
+  const accountsDict = new Map();
+  for (const account of state.accounts) {
+    if (account.ACCOUNT >= startAccount && account.ACCOUNT <= endAccount) {
+      accountsDict.set(account.ACCOUNT, account.LABEL);
+    }
+  }
+  const accountNumbers = [...accountsDict.keys()];
+
+  const balance = state.journalEntries
+    .filter((journal) => {
+      const isValidAccount = accountNumbers.includes(journal.ACCOUNT);
+      const isOnPeriod =
+        journal.PERIOD >= startPeriod && journal.PERIOD <= endPeriod;
+      return isValidAccount && isOnPeriod;
+    })
+    .reduce((acc, current) => {
+      const idx = acc.findIndex((v) => v.ACCOUNT === current.ACCOUNT);
+      if (idx === -1) {
+        current.BALANCE = current.DEBIT - current.CREDIT;
+        acc.push(current);
+      } else {
+        acc[idx].DEBIT += current.DEBIT;
+        acc[idx].CREDIT += current.CREDIT;
+        acc[idx].BALANCE = acc[idx].DEBIT - acc[idx].CREDIT;
+      }
+      return acc;
+    }, [])
+    .map((journal) => {
+      return {
+        ACCOUNT: journal.ACCOUNT,
+        DESCRIPTION: accountsDict.get(journal.ACCOUNT),
+        DEBIT: journal.DEBIT,
+        CREDIT: journal.CREDIT,
+        BALANCE: journal.BALANCE,
+      };
+    })
+    .sort((j1, j2) => j1.ACCOUNT - j2.ACCOUNT);
 
   const totalCredit = balance.reduce((acc, entry) => acc + entry.CREDIT, 0);
   const totalDebit = balance.reduce((acc, entry) => acc + entry.DEBIT, 0);
